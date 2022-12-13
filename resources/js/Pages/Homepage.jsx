0c82ebbdb92cdff0be396d3ac062af36485fa9b8
unknown <kikiirfansyah33@gmail.com>
2022-12-13 16:52:23 +0700

    Menambahkan daisy UI

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -1,13 +1,16 @@
+import NewsList from '@/Components/Homepage/NewsList';
+import Navbar from '@/Components/Navbar';
 import { Link, Head } from '@inertiajs/inertia-react';
 import React from 'react';
 
 export default function Homepage(props) {
     console.log(props)
     return (
-        <div className='flex justify-center items-center min-h-screen bg-slate-50'>
+        <div className='min-h-screen bg-slate-50'>
             <Head title={props.title} />
-            <div>
-                {props.news ? props.news.map((data, i) => {
+            <Navbar />
+            <NewsList news={props.news} />
+            {/* {props.news ? props.news.map((data, i) => {
                     return (
                         <div key={i} className="p-4 m-2 bg-white text-black shadow-md round-md border">
                             <p className='text-2xl'>{data.title}</p>
@@ -16,8 +19,8 @@ export default function Homepage(props) {
                             <i>{data.author}</i>
                         </div>
                     )
-                }) : <p>Saat ini belum ada berita tersedia</p>}
-            </div>
+                }) : <p>Saat ini belum ada berita tersedia</p>} */}
+
         </div>
     )
 }
